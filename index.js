5798ace0dc5fc573c521007fa62d9318d94eaf1d
victorramirez92 <victor.ramirez@alu.uhu.es>
2021-03-05 09:33:10 +0100

    3 files modified

--- index.js
+++ index.js
@@ -2,5 +2,4 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const container = document.querySelector('.container');
